a4c6a857c5eb09479175d210efeb4bc9ec5d57c8
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-12-11 12:02:30 -0500

    Local state counter example

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React from 'react'
+import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-    const loading = false
-    const showComments = true
-
-    if (loading) return <h1>Loading...</h1>
-
-    const commentBlock = ( 
-                                <div className="comments">
-                                <h3>Comments ({comments.length})</h3>
-                                <ul>
-                                    {comments.map((comment, index) => (
-                                        <li key={index}>
-                                            {comment.text}
-                                        </li>
-                                    ))}
-                                        </ul>
-                                    </div>
-                        )
-    return (
-        <div className="container">
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-
-            {showComments &&  commentBlock}
-
-       
-        </div>
-        
-  
+    return ( 
+        <>
+        <Header  />
+    <div className = "container" >
+            <FeedbackItem />
+    </div>
+        </>
     )
 }
 
