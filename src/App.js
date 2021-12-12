656cf547883c04c878069e7376cc5ab5f761d4ce
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-12-11 19:56:10 -0500

    Rating and Reviews dynamic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -17,6 +18,7 @@ function App() {
         <>
         <Header  />
     <div className = "container" >
+            <FeedbackStats feedback={feedback} />
             <FeedbackList 
                 feedback={feedback}
                 handleDelete={deleteFeedback}
