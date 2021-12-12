2bb245d20d7b7c92d9fde0787eda4e72b5794698
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-12-12 13:53:57 -0500

    Fully works adding feedback with rating and a little animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 
+
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
+    const [feedback, setFeedback] = useState(FeedbackData) 
+   
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+        
+    }
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item0) => item0.id !== id))
@@ -20,7 +28,7 @@ function App() {
         <Header  />
     <div className = "container" >
 
-            <FeedbackForm />
+            <FeedbackForm handleAdd={addFeedback} />
             <FeedbackStats feedback={feedback} />
             <FeedbackList 
                 feedback={feedback}
