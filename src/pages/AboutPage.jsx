78912681f9b5985ddc0336393c563037efa6fbc1
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-12-12 14:19:44 -0500

    Error with react-router-dom update was fixed

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+    return (
+        <Card>
+            <div className="about">
+                <h1>About This Project</h1>
+                <p>This is a Reach app to leaver feedback fro a product or service</p>
+                <p>Version: 1.0.0</p>
+
+                <p>
+                    <a href="/">Back To Home</a>
+                </p>
+            </div>
+        </Card>
+    )
+}
+
+export default AboutPage
