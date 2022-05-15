77583876350acfa71dd2b5c11090e09ad77bb446
Markiian Sanotskyi <marksanockiy@gmail.com>
2022-05-15 15:55:28 -0400

    Working with spinner

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
-import {FeedbackProvider} from './context/FeedbackContext'
+import { FeedbackProvider } from './context/FeedbackContext'
+
+
+
 
 
 function App() {
-    return ( 
-        <FeedbackProvider>
-        <Router>
-        <Header  />
-    <div className = "container" >
-        <Routes>
-        <Route exact path='/' element={
-            <>
-                <FeedbackForm />
-                <FeedbackStats />
-                <FeedbackList />
-            </>
-        }>
-          
-             </Route>
-           <Route path='/about' element={<AboutPage/>} />
-           
-           </Routes>
+    return ( <
+        FeedbackProvider >
+        <
+        Router >
+        <
+        Header / >
+        <
+        div className = "container" >
+        <
+        Routes >
+        <
+        Route exact path = '/'
+        element = { <
+            >
+            <
+            FeedbackForm / >
+            <
+            FeedbackStats / >
+            <
+            FeedbackList / >
+            <
+            />
+        } >
 
-         
+        <
+        /Route> <
+        Route path = '/about'
+        element = { < AboutPage / > }
+        />
 
-           <AboutIconLink />
-    </div> 
+        <
+        /Routes>
 
-        </Router>
-        </FeedbackProvider>
+
+
+        <
+        AboutIconLink / >
+        <
+        /div> 
+
+        <
+        /Router> < /
+        FeedbackProvider >
     )
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -13,28 +13,51 @@ function FeedbackList() {
     return <p>No Feedback Yet</p>
   }
 
-    return isLoading ? (
-        <Spinner />
-         ) : (
-        <div className="feedback-list">
-        <AnimatePresence>
-        {feedback.map((item) => (
-            <motion.div
-             key={item.id}
-             initial={{opacity: 0}}
-             animate={{opacity: 1}}
-             exit={{opacity: 0}}
+  return isLoading ? (
+    <Spinner />
+     ) : (
+    <div className="feedback-list">
+    <AnimatePresence>
+    {feedback.map((item) => (
+        <motion.div
+         key={item.id}
+         initial={{opacity: 0}}
+         animate={{opacity: 1}}
+         exit={{opacity: 0}}
+         
+         >
+            <FeedbackItem
+                key={item.id} 
+                item={item} 
+            />
+        </motion.div>
+    ))}
+    </AnimatePresence>
+</div>
+)
+
+    // return isLoading ? (
+    //     <Spinner />
+    //      ) : (
+    //     <div className="feedback-list">
+    //     <AnimatePresence>
+    //     {feedback.map((item) => (
+    //         <motion.div
+    //          key={item.id}
+    //          initial={{opacity: 0}}
+    //          animate={{opacity: 1}}
+    //          exit={{opacity: 0}}
              
-             >
-                <FeedbackItem
-                    key={item.id} 
-                    item={item} 
-                />
-            </motion.div>
-        ))}
-        </AnimatePresence>
-    </div>
-    )
+    //          >
+    //             <FeedbackItem
+    //                 key={item.id} 
+    //                 item={item} 
+    //             />
+    //         </motion.div>
+    //     ))}
+    //     </AnimatePresence>
+    // </div>
+    // )
   
 
     // return (
